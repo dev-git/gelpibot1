cf6b699ab6189cd0cb2f04fab5bb2c174b3add07
Paul Bates <dev-git@users.noreply.github.com>
2023-10-20 15:09:27 +1300

    Update module3.py

--- module3.py
+++ module3.py
@@ -1,16 +1,16 @@
 ##############################################
 # File Name: module3.py
-# Version: 1.0
-# Team No.: 20
+# Version: 1.1
+# Team No.: 1
 # Team Name:
-# Date: 22 Oct 15
+# Date: 20 Oct 23
 ##############################################
 
 import RPi.GPIO as GPIO
 import time
 import sys, tty, termios
 
-print '\nHi, I am PiBot, your very own learning robot...\n'
+print('\nHi, I am PiBot, your very own learning robot...\n')
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -19,12 +19,12 @@ GPIO.setup(7, GPIO.OUT)
 ledOnTime = 10
 
 # Turn on LED
-print 'Turn the LED on'
+print('Turn the LED on')
 GPIO.output(7, True)
 time.sleep(ledOnTime)
-print 'Turn the LED off'
+print('Turn the LED off')
 GPIO.output(7, False)
 
 GPIO.cleanup()
    
-print "\nEnd of program"
+print("\nEnd of program")
