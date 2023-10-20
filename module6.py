2d81a048eea4740e0231fbb1d2b78b2a1e6fa59c
Paul Bates <dev-git@users.noreply.github.com>
2023-10-20 15:15:53 +1300

    Update module6.py

--- module6.py
+++ module6.py
@@ -9,7 +9,7 @@
 import RPi.GPIO as GPIO
 import time
 
-print 'Programming the PiBot...'
+print('Programming the PiBot...')
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -20,30 +20,30 @@ GPIO.setup(15, GPIO.OUT)
 
 interval = 1
 
-print 'forward'
+print('forward')
 GPIO.output(11, True)
 GPIO.output(13, True)
 time.sleep(interval)
 GPIO.output(11, False)
 GPIO.output(13, False)
    
-print 'back'
+print('back')
 GPIO.output(7, True)
 GPIO.output(15, True)
 time.sleep(interval)
 GPIO.output(7, False)
 GPIO.output(15, False)
 
-print 'left'
+print('left')
 GPIO.output(13, True)
 time.sleep(interval)
 GPIO.output(13, False)
       
-print 'right'
+print('right')
 GPIO.output(11, True)
 time.sleep(interval)
 GPIO.output(11, False)
 
 GPIO.cleanup()
    
-print "\nPiBot is going offline..."
+print("\nPiBot is going offline...")
